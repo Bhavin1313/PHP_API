751632f349c5107d24993b806764a85bc442cb5c
jenishbabariya22 <jenishbabariya58@gmail.com>
2024-01-11 16:17:29 +0530

    Firat Commit

---
+++ Config/config.php
@@ -0,0 +1,35 @@
+<?php
+class Config{
+
+    public $HOSTNAME = "127.0.0.1";
+    public $USERNAME = "root";
+    public $PASSWORD = "";
+    public $DATABASE_NAME = "";
+    public $con_res;
+
+    public function Connect(){
+
+        $this->con_res = mysqli_connect($this->HOSTNAME,$this->USERNAME,$this->PASSWORD,$this->DATABASE_NAME);
+        return $this->con_res;
+    }
+
+    public function insertStudent($grid,$name,$course,$city){
+        $this->Connect();
+
+        $query = "INSERT INTO student_info(grid,name,course,city) VALUES ($grid,'$name','$course','$city');";
+
+        $res = mysqli_query($this->con_res,$query);
+        return $res;
+    }
+
+    public function fetchStudent(){
+        $this->Connect();
+
+        $query = "SELECT * FROM student_info";
+
+        $object = mysqli_query($this->con_res,$query);
+        return $object;
+    }
+
+}
+?>
